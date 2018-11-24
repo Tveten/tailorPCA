eb9072b6fa85bf719e46e8f52a59234d5b96955a
Martin <matveten@gmail.com>
2018-11-24 16:15:22 +0100

    Rewrote functions to avoid if statements.

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -5,13 +5,10 @@ is_whole_number <- function(x, tol = .Machine$double.eps^0.5)  {
 is_in_interval <- function(x, interval = c(0, 1)) {
   # Input:
   #   x: A single numeric.
-  if(!is.numeric(x)) return(FALSE)
-  if (x < interval[1] || x > interval[2]) return(FALSE)
-  else return(TRUE)
+  x >= interval[1] && x <= interval[2]
 }
 
 is_interval <- function(x) {
-  condition1 <- length(x == 2)
   if (length(x == 2)) {
     return(x[1] <= x[2])
   } else return(FALSE)
@@ -23,16 +20,14 @@ is_cor_mat <- function(cov_mat) {
 
 is_positive_definite <- function(cov_mat, tol = 1e-8) {
   eigen_values <- eigen(cov_mat, symmetric = TRUE, only.values = TRUE)$values
-  if (any(eigen_values < tol)) return(FALSE)
-  else return(TRUE)
+  all(eigen_values >= tol)
 }
 
 is_prob <- function(p) {
   # p: A vector  with probability weights.
   condition1 <- all(vapply(p, is_in_interval, logical(1), c(0, 1)))
   condition2 <- isTRUE(all.equal(sum(p), 1))
-  if (condition1 && condition2) return(TRUE)
-  else return(FALSE)
+  condition1 && condition2
 }
 
 interval_msg <- function(obj) {
