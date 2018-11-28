184dbdce450c90195f54128e8c17518c8d947ce5
Martin <matveten@gmail.com>
2018-11-28 15:39:09 +0100

    Test for attributes and sd changes.

--- tests/testthat/test_change_handlers.R
+++ tests/testthat/test_change_handlers.R
@@ -17,3 +17,35 @@ for (i in 1:100) {
                  2 * sum(1:(min(change_sparsity, K0) - 1)))
   })
 }
+
+N <- 20
+cor_mat <- generate_cor_mat(N, N - 1)
+attr(cor_mat, 'which_dims_cor') <- which_dims_cor(cor_mat)
+change_funcs <- set_uniform_cd(N)
+change_sparsity <- change_funcs$draw_sparsities(1)
+affected_dims <- change_funcs$draw_dims(change_sparsity)
+draw_cor <- change_funcs$draw_cor
+draw_sd <- change_funcs$draw_sd
+cor_mat_changed <- change_cor_mat(cor_mat, affected_dims,
+                                  draw_cor = draw_cor)
+test_that('Attributes of changed correlation matrix is correct', {
+  expect_identical(is.numeric(cor_mat), is.numeric(cor_mat_changed))
+  expect_identical(class(cor_mat), class(cor_mat_changed))
+  expect_identical(dim(cor_mat), dim(cor_mat_changed))
+})
+
+for (i in 1:100) {
+  N <- 10
+  K0 <- sample(2:N, 1)
+  cor_mat <- generate_cor_mat(N, K0)
+  attr(cor_mat, 'which_dims_cor') <- which_dims_cor(cor_mat)
+  change_funcs <- set_uniform_cd(N)
+  change_sparsity <- change_funcs$draw_sparsities(1)
+  affected_dims <- change_funcs$draw_dims(change_sparsity)
+  draw_sd <- change_funcs$draw_sd
+  cor_mat_changed <- change_cor_mat(cor_mat, affected_dims, draw_sd = draw_sd)
+  dims_changed <- (1:N)[which(diag(cor_mat) - diag(cor_mat_changed) != 0)]
+  test_that('Correct standard deviations changed', {
+    expect_true(all(dims_changed == sort(affected_dims)))
+  })
+}
