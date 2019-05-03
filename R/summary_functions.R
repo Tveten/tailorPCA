f6a7e108a25964f8c64eb72625450bf68051c9fd
Martin <matveten@gmail.com>
2019-05-03 18:18:59 +0200

    A minimal summary function.

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -1,3 +1,9 @@
 summary.tpca <- function(tpca_obj) {
-  tpca_obj[2:3] 
+  cat('Probability of projection j being the most sensitive (the projections are ordered decreasingly w.r.t variance): \n')
+  prop <- tpca_obj$prop_axes_max
+  names(prop) <- paste0("j = ", 1:length(prop))
+  print(prop)
+  cat('\n')
+  cat('Chosen axes/projections in decreasing order of sensitivity: \n')
+  cat(tpca_obj$which_axes)
 }
